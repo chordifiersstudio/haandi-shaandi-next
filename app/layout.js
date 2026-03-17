a80f399a7b9bd08749628df8469fc9d44db640de
chordifiersstudio <thechordifiersstudio@gmail.com>
2026-03-17 09:54:02 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -6,9 +6,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <script src="https://cdn.tailwindcss.com"></script>
-      </head>
       <body>{children}</body>
     </html>
   );
